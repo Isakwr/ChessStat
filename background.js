0fce9b5e94684c8a138c98951acd93703dd04210
Isakwr <isak.roenningen@gmail.com>
2023-12-25 03:08:32 +0100

    fixed filter to only work for youtube

--- background.js
+++ background.js
@@ -9,4 +9,4 @@ chrome.webRequest.onBeforeRequest.addListener(function(details){
     console.log("ALERT!!!!!!!!!!!!!!!!!!!!!!!!!HERE!!!!!!ALERT!!!!!");
     console.log("ALERT!!!!!!!!!!!!!!!!!!!!!!!!!HERE!!!!!!ALERT!!!!!");
     return;
-},{urls: ["<all_urls>"]},[]);
+},{urls: ["https://youtube.com"]},[]);
