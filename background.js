5d20b6724fc74b18a23f37acd1523efd34b75cc9
Isakwr <isak.roenningen@gmail.com>
2023-12-27 18:51:47 +0100

    chaanged url filter and added url on webNavigation.onCompleted documentId to correspond with documentId in onCompleted

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 let filter = {urls: ["https://www.youtube.com"]};
+const registeredUrls = new Map(["thisisurl", [0,1]]);
 
+chrome.webRequest.onCompleted.addListener(function(details){
+    registeredUrls.set(details.documentId, 1);
+    return;
+},{urls: ["https://www.youtube.com/*"]}),[];
 
 chrome.webNavigation.onCompleted.addListener(function(details){
-    console.log(`${details.documentId}`);
-    console.log(`${details.url}`);
+    if(registeredUrls.get(details.documentId) === 1){
+        console.log(`Already requested ${details.documentId}`);
+    }
     return;
 },{urls: ["*://www.youtube.com/*"]});
