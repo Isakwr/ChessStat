54fa617dd27101b3e4618a749301d8bb12a70cf9
Isakwr <isak.roenningen@gmail.com>
2023-12-27 19:15:16 +0100

    deleted webRequest.onCompleted

--- background.js
+++ background.js
@@ -1,14 +1,7 @@
 let filter = {urls: ["https://www.youtube.com"]};
-const registeredUrls = new Map([["thisisurl", [0,1]]]);
 
-chrome.webRequest.onCompleted.addListener(function(details){
-    registeredUrls.set(details.documentId, 1);
-    return;
-},{urls: ["https://www.youtube.com/*"]},[]);
 
 chrome.webNavigation.onCompleted.addListener(function(details){
-    if(registeredUrls.get(details.documentId) === 1){
-        console.log(`Already requested ${details.documentId}`);
-    }
+    console.log(`GOT THIS: ${details.documentId}`);
     return;
-},{urls: ["*://www.youtube.com/*"]});
+},{urls: ["https://www.youtube.com/*"]});
