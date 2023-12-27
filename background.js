792da50fb476db878eee8ee346c3caec2204b5a1
Isakwr <isak.roenningen@gmail.com>
2023-12-27 19:04:22 +0100

    fixed arguments in onCompleted

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ const registeredUrls = new Map([["thisisurl", [0,1]]]);
 chrome.webRequest.onCompleted.addListener(function(details){
     registeredUrls.set(details.documentId, 1);
     return;
-},{urls: ["https://www.youtube.com/*"]}),[];
+},{urls: ["https://www.youtube.com/*"]},[]);
 
 chrome.webNavigation.onCompleted.addListener(function(details){
     if(registeredUrls.get(details.documentId) === 1){
