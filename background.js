427b14daea4725ab35d00d04859488725feb5c5c
Isakwr <isak.roenningen@gmail.com>
2023-12-27 17:40:55 +0100

    added also print details.url

--- background.js
+++ background.js
@@ -3,6 +3,6 @@ let filter = {urls: ["https://www.youtube.com"]};
 
 chrome.webNavigation.onCompleted.addListener(function(details){
     console.log(`${details.documentId}`);
-    console.log("hey");
+    console.log(`${details.url}`);
     return;
 },{urls: ["*://www.youtube.com/*"]});
